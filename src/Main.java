46f0cb705c8fcce8ce42dfb7a55365cfecbd411d
Konstantin Strümpf <k.struempf@icloud.com>
2020-01-05 22:43:07 +0100

    Listing all midi files, user file selection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static helper.StaticFileHelper.selectMidiFile;
+
 public class Main {
 
+    private static final InputStream inStream = System.in;
+    private static final PrintStream outStream = System.out;
+    private static final PrintStream errStream = System.err;
+
     public static void main(String[] args) {
-        // write your code here
+        var midiDirectory = new File(args[0]);
+
+        if (args.length != 1) {
+            printUsage(outStream);
+            System.exit(1);
+        }
+
+        outStream.println(midiDirectory.getAbsolutePath());
+
+        if (!midiDirectory.isDirectory()) {
+            errStream.printf("Directory %s not found", midiDirectory);
+            System.exit(1);
+        }
+
+        var selectedFile = selectMidiFile(midiDirectory, inStream, outStream);
+
+        if (selectedFile == null) {
+            System.exit(1);
+        }
+
+        outStream.printf("Selected file %s\n", selectedFile.getName());
     }
+
+    private static void printUsage(PrintStream stream) {
+        stream.print("program directory");
+    }
+
 }
---
+++ src/helper/StaticFileHelper.java
@@ -0,0 +1,50 @@
+package helper;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class StaticFileHelper {
+
+    /**
+     * Get's a collection of midi files in a specified directory.
+     * Subdirectories will be ignored.
+     *
+     * @param directory The directory that will be searched (non recursive).
+     * @return A list of filed with a .mid extension.
+     */
+    public static List<File> getMidiFilesInDirectory(final File directory) {
+        var midiFiles = new LinkedList<File>();
+        for (final File fileEntry : Objects.requireNonNull(directory.listFiles())) {
+            if (!fileEntry.isDirectory() && fileEntry.getName().endsWith(".mid")) {
+                midiFiles.add(fileEntry);
+            }
+        }
+        return midiFiles;
+    }
+
+    public static File selectMidiFile(File midiDirectory, InputStream inputStream, PrintStream outStream) {
+        outStream.println("Available files:");
+        var midiFiles = StaticFileHelper.getMidiFilesInDirectory(midiDirectory);
+        var scanner = new Scanner(inputStream);
+        int i = 0;
+
+        for (File midiFile : midiFiles) {
+            outStream.printf("\t[%d] %s\n", i, midiFile.getName());
+            i++;
+        }
+
+        outStream.print("Select a midi file to read: ");
+        var userInput = scanner.nextInt();
+
+        if (userInput < 0 || userInput >= midiFiles.size()) {
+            return null;
+        }
+
+        return midiFiles.get(userInput);
+    }
+}
